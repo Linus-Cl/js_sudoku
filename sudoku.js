13ca2ad268f9195aecb76ab44656f81e3cfa7957
y0105877 <l.claussen@tu-braunschweig.de>
2023-10-04 16:53:48 +0200

    added eval row and collumn

--- sudoku.js
+++ sudoku.js
@@ -10,6 +10,18 @@ let board = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+let board2 = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [2, 3, 4, 5, 6, 7, 8, 9, 1],
+    [3, 4, 5, 6, 7, 8, 9, 1, 2],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [5, 6, 7, 8, 9, 1, 2, 3, 4],
+    [6, 7, 8, 9, 1, 2, 3, 4, 5],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [8, 9, 1, 2, 3, 4, 5, 0, 7],
+    [9, 1, 2, 3, 4, 5, 6, 7, 8],
+]
+
 let activeTile = null;
 
 window.onload = (event) => {
@@ -63,4 +75,45 @@ function updateBoard(id, num) {
     const coords = id.split("-");
     board[coords[0]][coords[1]] = num;
     //console.log(board);
+}
+
+function evaluateBoard() {
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (board[r][c] === 0) {
+                return false;
+            }
+            if (!evaluateRow(r, c) ||
+                !evaluateCollumn(r, c) /*&&
+                evaluateBlock(r, c)*/) {
+                return false;
+            }
+
+        }
+    }
+    return true;
+}
+
+function evaluateRow(row, collumn) {
+    for (let i = 0; i < 9; i++) {
+        if (i === collumn) {
+            continue;
+        }
+        if (board[row][i] === board[row][collumn]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function evaluateCollumn(row, collumn) {
+    for (let i = 0; i < 9; i++) {
+        if (i === row) {
+            continue;
+        }
+        if (board[i][collumn] === board[row][collumn]) {
+            return false;
+        }
+    }
+    return true;
 }
